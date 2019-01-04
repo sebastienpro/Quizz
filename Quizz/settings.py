f3c842a6fb1b1e1d3bf1aec47e2361f1d893b43e
sebastienpro <spro@webgeoservices.com>
2019-01-04 14:48:30 +0100

    Create first models and configure postgres

--- Quizz/settings.py
+++ Quizz/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'Quizz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizz',
+        'USER': 'quizz',
+        'PASSWORD': 'nopass',
+        'HOST': 'pgquizz',
+        'PORT': '5432',
     }
 }
 
--- quizzer/models.py
+++ quizzer/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Quizz(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Round(models.Model):
+    name = models.CharField(max_length=255)
+    quizz = models.ForeignKey(Quizz, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    TYPE_CHOICES = (
+        ('txt', 'texte'),
+        ('img', 'image'),
+        ('vid', 'video')
+    )
+    question = models.TextField(null=False)
+    answer = models.TextField(null=True, default=None)
+    type = models.CharField(max_length=3, choices=TYPE_CHOICES)
+    order = models.IntegerField(default=0)
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Participate(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    quizz = models.ForeignKey(Quizz, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.quizz.name+" / "+self.team.name
