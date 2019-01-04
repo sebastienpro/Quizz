dccee29454ec58a811a80c0ca401b8fb81ba66e2
Samuel Mendes <samuel.mendes@gmail.com>
2019-01-04 15:05:13 +0100

    Initial migration and admin setup

--- quizzer/admin.py
+++ quizzer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quizz, Round, Question, Team, Participate
+
+admin.site.register(Quizz)
+admin.site.register(Round)
+admin.site.register(Question)
+admin.site.register(Team)
+admin.site.register(Participate)
